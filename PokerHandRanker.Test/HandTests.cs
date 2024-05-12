aeb21d17e4b3fcf50cc5c4d7918955bf9d230c11
Abel Meijberg <ameijberg@gmail.com>
2024-05-12 09:35:47 -0400

    Added first unit test

---
+++ PokerHandRanker.Test/HandTests.cs
@@ -0,0 +1,38 @@
+﻿using PokerHandRanker;
+
+namespace PokerHandRanker.Test;
+
+public class HandTests
+{
+    [Fact]
+    public void CheckFlush_ReturnsTrue_WhenAllSuitsAreSame()
+    {
+        Card[] cards = 
+        {
+            new(Rank.Ace, Suit.Hearts),
+            new(Rank.Two, Suit.Hearts),
+            new(Rank.Three, Suit.Hearts),
+            new(Rank.Four, Suit.Hearts),
+            new(Rank.Five, Suit.Hearts),
+        };
+        Hand hand = new Hand(cards);
+
+        Assert.True(hand.IsFLush);
+    }
+
+    [Fact]
+    public void CheckFlush_ReturnsFalse_WhenNotAllSuitsAreSame()
+    {
+        Card[] cards =
+        {
+            new(Rank.Ace, Suit.Hearts),
+            new(Rank.Two, Suit.Hearts),
+            new(Rank.Three, Suit.Diamonds),
+            new(Rank.Four, Suit.Hearts),
+            new(Rank.Five, Suit.Hearts),
+        };
+        var hand = new Hand(cards);
+        
+        Assert.False(hand.IsFLush);
+    }
+}
